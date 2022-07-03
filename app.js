51956a309f26e8c24dd69e03a3a98241c6748b59
suzanne <suzannehcurtis@gmail.com>
2022-07-03 17:56:26 +0100

    md5 version

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
+ 
+console.log(md5('message'));
+
 
 
 const app = express();
@@ -24,10 +27,6 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res) {
@@ -45,7 +44,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
     const newUser = new User ({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
@@ -59,7 +58,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser) {
         if (err) {
